a01b7a5ae71b2f69980e052c671eb83510bc0844
Unix-Code <dmalakh@comcast.net>
2015-11-18 18:12:51 -0500

    All BlueJ files

--- NonConsecArray.java
+++ NonConsecArray.java
@@ -2,24 +2,23 @@ public class NonConsecArray {
     public int sum(int[] array) {
         int Sum = 0;
         int i = 0;
-        if (array.length%2 != 0) {
-            int iTemp = 2;
-            int tempSum = 0;
-            while (i < array.length) {
-                if (i + iTemp >= array.length - 1) {
-                    iTemp++;
-                    i = 0;
-                }
-                else {
-                    tempSum = array[i] + array[i + iTemp];
-                }
-                
-                if (tempSum > Sum) {
-                    Sum = tempSum;
-                }
-                i++;
-            } 
-        }
+        int iTemp = 2;
+        int tempSum = 0;
+        
+        while (i < array.length && iTemp < array.length) {
+            if (i + iTemp > array.length - 1) {
+                iTemp++;
+                i = 0;
+            }
+            else {
+                tempSum = array[i] + array[i + iTemp];
+            }
+
+            if (tempSum > Sum) {
+                Sum = tempSum;
+            }
+            i++;
+        } 
 
         return Sum;
     }
